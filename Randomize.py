f8caaaad370e9477387fc7111b769583675a1c97
1234567890num <53823593+1234567890num@users.noreply.github.com>
2022-02-14 17:24:43 +0700

    Initial Upload

---
+++ Randomize.py
@@ -0,0 +1,51 @@
+import os
+import json
+import random
+
+#Functions to write stuff
+def writemdlx(): #Avoid conflict with Wisdom Form
+    f.write('- name: obj/'+old+'.mdlx\n  method: binarc\n  source:\n')
+    f.write('  - name: p_ex\n    type: model\n    method: copy\n    source:\n'+
+            '    - name: obj/'+new+'.model\n') #Model
+    f.write('  - name: tim_\n    type: modeltexture\n    method: copy\n    source:\n'+
+            '    - name: obj/'+new+'.tim\n') #Texture
+def writeafm(sqd=False):
+    f.write('- name: obj/'+old+'.a.fm\n  method: binarc\n  source:\n')
+    f.write('  - name: face\n    type: imgd\n    method: copy\n    source:\n'+
+            '    - name: obj/'+new+'.imd\n') #Portrait
+    f.write('  - name: face\n    type: seqd\n    method: copy\n    source:\n'+
+            '    - name: obj/'+new+'.sqd\n') #Portrait Location
+
+#Get KH2 model filenames
+currentDir = os.path.realpath(__file__).replace(os.path.basename(__file__),'')
+f = open(currentDir+'modellist.json','r')
+models   = json.load(f)
+oldsora  = models['Sora']
+olddolan = models['Donald']
+oldgoffy = models['Goofy']
+f.close()
+
+newsora  = random.sample(oldsora ,len(oldsora ))
+newdolan = random.sample(olddolan,len(olddolan))
+newgoffy = random.sample(oldgoffy,len(oldgoffy))
+
+#Write the mod.yml
+f = open(currentDir+'mod.yml','w',encoding='utf-8')
+f.write('assets:\n')
+for i in range(len(oldsora)):
+    old = oldsora[i]
+    new = newsora[i]
+    writemdlx()
+    writeafm(True)
+for i in range(len(olddolan)):
+    old = olddolan[i]
+    new = newdolan[i]
+    writemdlx()
+    writeafm()
+for i in range(len(oldgoffy)):
+    old = oldgoffy[i]
+    new = newgoffy[i]
+    writemdlx()
+    writeafm()
+
+f.close()
