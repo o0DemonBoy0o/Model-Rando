89fd1902bbe81323851865412fdb3d418b5e22b4
1234567890num <53823593+1234567890num@users.noreply.github.com>
2022-02-19 00:24:52 +0700

    Added more randomization

--- Randomize.py
+++ Randomize.py
@@ -3,49 +3,42 @@ import json
 import random
 
 #Functions to write stuff
-def writemdlx(): #Avoid conflict with Wisdom Form
+def writemodel(old,new):
+    #MDLX (Avoid conflict with Wisdom Form)
     f.write('- name: obj/'+old+'.mdlx\n  method: binarc\n  source:\n')
-    f.write('  - name: p_ex\n    type: model\n    method: copy\n    source:\n'+
+    f.write('  - name: '+subfile+'\n    type: model\n    method: copy\n    source:\n'+
             '    - name: obj/'+new+'.model\n') #Model
     f.write('  - name: tim_\n    type: modeltexture\n    method: copy\n    source:\n'+
             '    - name: obj/'+new+'.tim\n') #Texture
-def writeafm():
+    #A.FM
+    if old[:4] == 'W_EX':
+        f.write('- name: obj/'+old+'.a.fm\n  method: copy\n  source:\n'+
+                '  - name: obj/'+new+'.a.fm\n')
+        return
     f.write('- name: obj/'+old+'.a.fm\n  method: binarc\n  source:\n')
     f.write('  - name: face\n    type: imgd\n    method: copy\n    source:\n'+
             '    - name: obj/'+new+'.imd\n') #Portrait
     f.write('  - name: face\n    type: seqd\n    method: copy\n    source:\n'+
             '    - name: obj/'+new+'.sqd\n') #Portrait Location
 
+def randomodel(oldmodel):
+    global subfile
+    newmodel = random.sample(oldmodel,len(oldmodel))
+    for i in range(len(oldmodel)):
+        subfile = oldmodel[i][:4].lower()
+        writemodel(oldmodel[i],newmodel[i])
+
 #Get KH2 model filenames
 currentDir = sys.argv[0].replace((sys.argv[0].split('\\')[-1]),'')
 f = open(currentDir+'modellist.json','r')
 models   = json.load(f)
-oldsora  = models['Sora']
-olddolan = models['Donald']
-oldgoffy = models['Goofy']
 f.close()
-
-newsora  = random.sample(oldsora ,len(oldsora ))
-newdolan = random.sample(olddolan,len(olddolan))
-newgoffy = random.sample(oldgoffy,len(oldgoffy))
 
 #Write the mod.yml
 f = open(currentDir+'mod.yml','w',encoding='utf-8')
 f.write('assets:\n')
-for i in range(len(oldsora)):
-    old = oldsora[i]
-    new = newsora[i]
-    writemdlx()
-    writeafm()
-for i in range(len(olddolan)):
-    old = olddolan[i]
-    new = newdolan[i]
-    writemdlx()
-    writeafm()
-for i in range(len(oldgoffy)):
-    old = oldgoffy[i]
-    new = newgoffy[i]
-    writemdlx()
-    writeafm()
+for char in models:
+    model = models[char]
+    randomodel(model)
 
 f.close()
