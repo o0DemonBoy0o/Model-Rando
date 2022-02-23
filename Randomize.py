8656dc0a0a40f8230af3fb9dbf7b60e91b1f03e1
1234567890num <53823593+1234567890num@users.noreply.github.com>
2022-02-23 22:50:43 +0700

    Overhauled json and added chest & enemy model rando

--- Randomize.py
+++ Randomize.py
@@ -6,20 +6,22 @@ import random
 def writemodel(old,new):
     #MDLX (Avoid conflict with Wisdom Form)
     f.write('- name: obj/'+old+'.mdlx\n  method: binarc\n  source:\n')
-    f.write('  - name: '+subfile+'\n    type: model\n    method: copy\n    source:\n'+
+    if objtype != 'Enemy':
+        subfile = old[:4].lower()
+        f.write('  - name: '+subfile+'\n    type: model\n    method: copy\n    source:\n'+
             '    - name: obj/'+new+'.model\n') #Model
     f.write('  - name: tim_\n    type: modeltexture\n    method: copy\n    source:\n'+
             '    - name: obj/'+new+'.tim\n') #Texture
     #A.FM
-    if old[:4] == 'W_EX':
+    if objtype == 'Party':
+        f.write('- name: obj/'+old+'.a.fm\n  method: binarc\n  source:\n')
+        f.write('  - name: face\n    type: imgd\n    method: copy\n    source:\n'+
+                '    - name: obj/'+new+'.imd\n') #Portrait
+        f.write('  - name: face\n    type: seqd\n    method: copy\n    source:\n'+
+                '    - name: obj/'+new+'.sqd\n') #Portrait Location
+    elif objtype == 'Weapon':
         f.write('- name: obj/'+old+'.a.fm\n  method: copy\n  source:\n'+
-                '  - name: obj/'+new+'.a.fm\n')
-        return
-    f.write('- name: obj/'+old+'.a.fm\n  method: binarc\n  source:\n')
-    f.write('  - name: face\n    type: imgd\n    method: copy\n    source:\n'+
-            '    - name: obj/'+new+'.imd\n') #Portrait
-    f.write('  - name: face\n    type: seqd\n    method: copy\n    source:\n'+
-            '    - name: obj/'+new+'.sqd\n') #Portrait Location
+                '  - name: obj/'+new+'.a.fm\n') #Particle & Sound Effect
 
 def randomodel(oldmodel):
     global subfile
@@ -30,15 +32,20 @@ def randomodel(oldmodel):
 
 #Get KH2 model filenames
 currentDir = sys.argv[0].replace((sys.argv[0].split('\\')[-1]),'')
-f = open(currentDir+'modellist.json','r')
-models   = json.load(f)
-f.close()
+objs = json.load(open(currentDir+'modellist.json'))
+objsextra = json.load(open(currentDir+'modellistextra.json'))
 
 #Write the mod.yml
 f = open(currentDir+'mod.yml','w',encoding='utf-8')
+f.write('description: Credits for PandaPyre for Final MiXaV Retexture\n')
 f.write('assets:\n')
-for char in models:
-    model = models[char]
-    randomodel(model)
+for objtype in objs:
+    obj = objs[objtype]
+    for models in obj:
+        oldmodels = objs[objtype][models]
+        newmodels = oldmodels + objsextra[objtype][models]
+        random.shuffle(newmodels)
+        for i in range(len(oldmodels)):
+            writemodel(oldmodels[i],newmodels[i])
 
 f.close()
